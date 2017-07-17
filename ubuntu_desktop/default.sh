78d214f2badb6d792508e1ac263b400685324a36
kenta-s <knt01222@gmail.com>
2017-07-17 13:18:57 +0900

    Add gnome settings

--- ubuntu_desktop/default.sh
+++ ubuntu_desktop/default.sh
@@ -4,6 +4,7 @@ sudo apt-get update -y
 sudo apt-get install vim -y
 sudo apt-get install wget -y
 sudo apt-get install git -y
+sudo apt-get install gnome-tweak-tool -y
 
 git config --global user.email "knt01222@gmail.com"
 git config --global user.name "kenta-s"

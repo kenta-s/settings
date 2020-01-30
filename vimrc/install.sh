fc2a7564457834a4960fd579a7544985be97a340
kenta-s <kenta.shirai@ascenderhcm.com>
2020-01-30 17:01:36 +0900

    add molokai.vim

--- vimrc/install.sh
+++ vimrc/install.sh
@@ -6,4 +6,6 @@ mkdir -p ~/.vim/bundles
 bash $script_dir/installer.sh ~/.vim/bundles
 ln -s $script_dir/vimrc ~/.vimrc
 rm $script_dir/installer.sh
+mkdir -p ~/.vim/colors
+ln -s $script_dir/colors/molokai.vim ~/.vim/colors/molokai.vim
 ln -s $script_dir/../tmux/.tmux.conf ~/.tmux.conf
